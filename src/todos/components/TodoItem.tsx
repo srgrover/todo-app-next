f4c9b82c1a3c940d8630037a775c5dd5a110d0c0
Jonathan Moya <djgrovezone@gmail.com>
2025-03-03 18:32:32 +0100

    Enhance TodoItem component with improved styling and icons for completion status

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,11 +1,34 @@
-import { Todo } from "@prisma/client"
+import { Todo } from "@prisma/client";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
-  todo: Todo
+  todo: Todo;
 }
 
 export const TodoItem = ({ todo }: Props) => {
   return (
-    <div>{todo.description}</div>
+    <div className={`rounded-lg shadow-sm p-5 border-dashed border flex 
+      flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0 
+      ${
+        todo.complete
+        ? 'line-through bg-blue-50 border-blue-500'
+        : 'bg-red-50 border-red-500'
+      }`}>
+      <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
+        <div className={`flex p-2 rounded-md cursor-pointer 
+          hover:bg-opacity-60 bg-blue-100 border-b-gray-600
+          ${ todo.complete ? 'tebgxt-blue-100' : 'bg-red-100' }
+        `}>
+            {
+              todo.complete ?
+              <IoCheckboxOutline size={30} /> :
+              <IoSquareOutline size={30} />
+            }
+        </div>
+        <div className="text-center sm:text-left">
+          { todo.description }
+        </div>
+      </div>
+    </div>
   )
 }
