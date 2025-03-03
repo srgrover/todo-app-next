d3a395dc64947ad3abf6a2f31d9abee6e7f3fc19
Jonathan <jonathan.moya@getronics.com>
2025-03-03 13:30:06 +0100

    Add remote image patterns for Tailus and Gravatar in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailus.io'
+      },
+    {
+        protocol: 'https',
+        hostname: 'gravatar.com'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
